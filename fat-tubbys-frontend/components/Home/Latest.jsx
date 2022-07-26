45523b50f582516140eabd4981b786f8bdcb9768
unknown <aayushd@iitbhilai.ac.in>
2022-07-26 16:09:41 +0530

    Add Searchbar and section tabs

--- fat-tubbys-frontend/components/Home/Latest.jsx
+++ fat-tubbys-frontend/components/Home/Latest.jsx
@@ -4,6 +4,15 @@ const Latest = () => {
   return (
     <>
       <div className="w-11/12 h-auto p-6 mx-auto">
+        <div className="form-control w-6/12 mx-auto mt-3 mb-8">
+          <input type="text" placeholder="Search" className="input input-bordered" />
+        </div>
+        <div className=" flex flex-col items-center my-5">
+          <div className="tabs tabs-boxed">
+            <a className="tab tab-lg tab-bordered mx-6">Available</a>
+            <a className="tab tab-lg tab-bordered tab-active text-white">Rented</a>
+          </div>
+        </div>
         <div className="flex flex-row justify-center space-x-4 items-center">
           <_card
             title="Room 55"
